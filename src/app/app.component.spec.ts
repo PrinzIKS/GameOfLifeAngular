a768eef3505cfc1a50629f405f32a772ee4e408c
Prinz <fpr@iks-gmbh.com>
2022-03-16 14:59:09 +0100

    implemented logi

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,24 +13,108 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    const fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GameOfLifeAngular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('GameOfLifeAngular');
+  it(`should have as title 'Game Of Life'`, () => {
+    expect(app.title).toEqual('Game Of Life');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('GameOfLifeAngular app is running!');
+  it(`should have as maxRounds 10'`, () => {
+    expect(app.maxRounds).toEqual(10);
   });
+
+  it(`should have as live  true after start'`, () => {
+    expect(app.live).toEqual(false);
+    app.start();
+    expect(app.live).toEqual(true);
+  });
+
+  it(`centered dead cell should remain dead if no neighbour'`, () => {
+    app.field = [[false, false, false], [false, false, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered alive cell should die if no neighbour'`, () => {
+    app.field = [[false, false, false], [false, true, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered alive cell should die if one neighbour'`, () => {
+    app.field = [[false, true, false], [false, true, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered alive cell should live if two neighbours'`, () => {
+    app.field = [[false, true, false], [false, true, false], [false, true, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(true);
+  });
+
+  it(`centered alive cell should live if two neighbours 2'`, () => {
+    app.field = [[true, true, false], [false, true, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(true);
+  });
+
+  it(`centered dead cell should remain dead if two neighbours'`, () => {
+    app.field = [[true, true, false], [false, false, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered alive cell should live if three neighbours'`, () => {
+    app.field = [[true, true, true], [false, true, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(true);
+  });
+
+  it(`centered dead cell should live if three neighbours'`, () => {
+    app.field = [[true, true, true], [false, false, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(true);
+  });
+
+  it(`centered alive cell should die if four neighbours'`, () => {
+    app.field = [[true, true, true], [false, true, false], [false, true, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered alive cell should die if eight neighbours'`, () => {
+    app.field = [[true, true, true], [true, true, true], [true, true, true]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered dead cell should die if four neighbours'`, () => {
+    app.field = [[true, true, true], [false, false, false], [false, true, false]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`centered dead cell should remain dead if eight neighbours'`, () => {
+    app.field = [
+      [true, true, true],
+      [true, false, true],
+      [true, true, true]];
+    app.nextGeneration();
+    expect(app.field[1][1]).toEqual(false);
+  });
+
+  it(`?'`, () => {
+    app.field = [[false, true, false], [false, true, false], [false, false, false]];
+    app.nextGeneration();
+    expect(app.field[0][1]).toEqual(false);
+    expect(app.field[1][1]).toEqual(false);
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,152 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GameOfLifeAngular';
+export class AppComponent implements OnInit {
+  title = 'Game Of Life';
+  field: boolean[][] = [];
+  rowCount = 5;
+  colCount = 10;
+  round = 1;
+  maxRounds = 10;
+  speed = 1000;
+  live = false;
+
+  ngOnInit() {
+    this.setupField();
+  }
+
+  setupField(indices?: number[][]) {
+    this.round = 1;
+    this.field = [];
+    for (let i = 0; i < this.rowCount; i++) {
+      let row: boolean[] = [];
+      for (let i = 0; i < this.colCount; i++) {
+        row.push(false);
+      }
+      this.field.push(row)
+    }
+    if (indices) {
+      for (let i = 0; i < indices.length; i++) {
+        for (let j = 0; j < indices[i].length; j++) {
+          this.switchState(i, indices[i][j]);
+        }
+      }
+    }
+  }
+
+  switchState(row: number, col: number) {
+    if (this.live == false)
+      this.field[row][col] = !this.field[row][col];
+  }
+
+  start() {
+    this.live = true;
+    this.play();
+  }
+
+  stop() {
+    this.live = false;
+  }
+
+  play() {
+    if (this.live)
+      setTimeout(() => {
+        this.nextGeneration();
+        this.round++;
+        if (this.round < this.maxRounds) {
+          this.play();
+        }
+      }, this.speed);
+  }
+
+  setupGosperGliderGun() {
+    this.rowCount = 26;
+    this.colCount = 38;
+    this.maxRounds = 40;
+    this.speed = 100;
+    let indices = [
+      [],
+      [25],
+      [23, 25],
+      [13, 14, 21, 22, 35, 36],
+      [12, 16, 21, 22, 35, 36],
+      [1, 2, 11, 17, 21, 22],
+      [1, 2, 11, 15, 17, 18, 23, 25],
+      [11, 17, 25],
+      [12, 16],
+      [13, 14],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ];
+    this.setupField(indices);
+  }
+
+  setupSailors() {
+    this.rowCount = 13;
+    this.colCount = 60;
+    this.maxRounds = 40;
+    this.speed = 100;
+    let indices = [
+      [],
+      [],
+      [],
+      [],
+      [4, 5, 6, 7, 13, 14, 15, 16, 17, 23, 24, 25, 26, 27, 28],
+      [3, 7, 12, 17, 22, 28],
+      [7, 17, 28],
+      [3, 6, 12, 16, 22, 27],
+      [14, 24, 25],
+      [],
+      [],
+      [],
+      []
+    ];
+    this.setupField(indices);
+  }
+
+  nextGeneration() {
+    let nextGeneration: boolean[][] = [];
+    for (let i = 0; i < this.field.length; i++) {
+      let nextRow: boolean[] = [];
+      for (let j = 0; j < this.field[i].length; j++) {
+        let amountAliveNeighbours = this.countAliveNeighbours(i, j);
+        nextRow[j] = (amountAliveNeighbours == 2 && this.field[i][j]) || amountAliveNeighbours == 3;
+      }
+      nextGeneration[i] = nextRow;
+    }
+    this.field = nextGeneration;
+  }
+
+  private countAliveNeighbours(y: number, x: number): number {
+    let amountAliveNeighbours = 0;
+    let rowLowerLimit = y == 0 ? 0 : y - 1;
+    let rowUpperLimit = y == this.field.length - 1 ? this.field.length - 1 : y + 1;
+    let colLowerLimit = x == 0 ? 0 : x - 1
+    let colUpperLimit = x == this.field[0].length - 1 ? this.field[0].length - 1 : x + 1;
+    for (let i = rowLowerLimit; i <= rowUpperLimit; i++) {
+      for (let j = colLowerLimit; j <= colUpperLimit; j++) {
+        if (this.field[i][j] && !(i == y && j == x)) amountAliveNeighbours++;
+      }
+    }
+    return amountAliveNeighbours;
+  }
+
 }
